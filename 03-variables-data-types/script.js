bed97784717e41c6d14d2934229a9ad41b3dedc4
Jason <jasonetienne1@gmail.com>
2023-09-08 18:08:29 +0100

    qwerty

---
+++ 03-variables-data-types/script.js
@@ -0,0 +1,67 @@
+ 
+
+ let firstName = 'Jason';
+ let lastName = 'Etienne';
+
+ console.log(firstName, lastName);
+
+
+ let age = 39;
+ console.log(age);
+
+
+ // Re-assigning variables
+ age = 41
+ console.log(age)
+
+
+ // Declare without assigning only with let doesnt work with const
+ let score;
+
+
+ score = 16
+ console.log(score)
+
+ if (true) {
+    score = score+1
+ }
+
+ console.log(score)
+
+ const arr = [1,2,3,4]
+ arr.push(5)
+console.log(arr)
+
+
+
+const person ={
+    name: 'jason'
+};
+
+
+person.name = 'nick'
+
+console.log(person)
+
+person.email = 'jason@mail.com'
+console.log(person)
+
+// Declare multiple variables at once
+let a,b,c;
+
+const d = 10, e = 15, f = 20; 
+console.log(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
