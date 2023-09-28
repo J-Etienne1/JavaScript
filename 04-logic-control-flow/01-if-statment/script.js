de68d1ff91a6b60d0235210741e3a018ee507154
Jason <jasonetienne1@gmail.com>
2023-09-28 17:47:27 +0100

    .

---
+++ 04-logic-control-flow/01-if-statment/script.js
@@ -0,0 +1,77 @@
+
+
+if (true) {
+    console.log("This is True")
+}
+
+
+if (false) {
+    console.log("This is fasle")
+}
+
+
+const x = 10;
+const y = 5;
+
+if (x > y) {
+    console.log(`${x} is greater than ${y}`)
+}
+
+
+if (x === y) { // wont run as is False
+    console.log(`${x} is not equal to ${y}`)
+}
+
+
+
+
+
+
+if (x === y) {
+    console.log(`${x} is equal to ${y}`)
+} else {
+    console.log(`${x} is NOT equal to ${y}`)
+}
+
+
+
+
+if (x != y) {
+    const z = 20;
+    console.log(`${z} is 20`)
+}
+// console.log(z) z cant be accessed here, due to z only been in the block scope for the If statment
+
+
+
+
+
+// Shorthand If  (not recomended)
+if (x > y)  console.log(`${x} is greater than ${y}`);
+
+// Shorthand else (not recomended)
+if (x > y)  console.log(`${x} is greater than ${y}`);
+else console.log("This is false")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
