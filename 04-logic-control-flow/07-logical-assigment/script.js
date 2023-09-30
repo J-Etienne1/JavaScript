45dec3f6049880f29b9c6cfdc6402d780c295587
Jason <jasonetienne1@gmail.com>
2023-09-30 16:50:14 +0100

    a

---
+++ 04-logic-control-flow/07-logical-assigment/script.js
@@ -0,0 +1,14 @@
+
+// || assigns the right side value only if the left is a False value
+
+
+
+
+
+// && assigns the right side value only if the left is a True value
+
+
+
+
+
+// ?? assigns the right side value only if the left is a Null OR Undedfinded
