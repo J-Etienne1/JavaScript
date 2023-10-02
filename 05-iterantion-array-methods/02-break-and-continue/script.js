27e387b6918b6a748fd6b3e4819db6942d99f2b9
Jason <jasonetienne1@gmail.com>
2023-10-02 18:23:49 +0100

    .

---
+++ 05-iterantion-array-methods/02-break-and-continue/script.js
@@ -0,0 +1,22 @@
+
+
+// Break
+for (let i = 0; i <= 20; i++) {
+    if (i === 15) {
+        console.log("Braking...")
+        break;
+    }
+    console.log(i);
+    
+}
+
+
+
+// Continue
+for ( let j = 0; j <= 30; j++) {   
+    if (j === 13) {
+        console.log("Skipping 13...")
+        continue
+    }
+    console.log(j)
+}
