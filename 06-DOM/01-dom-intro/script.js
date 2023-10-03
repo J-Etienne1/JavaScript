d1409b49a4aef0bf7f8c108747ba6da373ec9707
Jason <jasonetienne1@gmail.com>
2023-10-03 19:11:54 +0100

    .

---
+++ 06-DOM/01-dom-intro/script.js
@@ -0,0 +1,34 @@
+//console.log(window)
+console.log(window.document)
+console.dir(window.document) // see proprties and methods
+
+
+
+console.log(document.body);
+
+
+console.log(document.body.innerHTML);
+console.log(document.body.innerText);
+
+
+
+console.log(document.links);
+console.log(document.links[0]);
+
+
+
+//document.body.innerHTML = "<h1>Hello World<h1>"
+
+
+
+// document.write("Hello from JS")
+
+
+console.log(document.getElementById("main"))
+const main = document.getElementById("main")
+// main.innerHTML = "<h1>Hello from main<h1>"
+
+document.querySelector("#main h1").innerText = "Hello2"
+
+
+
