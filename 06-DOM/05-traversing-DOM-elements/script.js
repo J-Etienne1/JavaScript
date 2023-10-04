6d42b178a444f1c7c24477d8407585c7c297497b
Jason <jasonetienne1@gmail.com>
2023-10-04 19:08:20 +0100

    .

---
+++ 06-DOM/05-traversing-DOM-elements/script.js
@@ -0,0 +1,52 @@
+let output;
+
+// Get Child elements
+const parent = document.querySelector(".parent")
+output = parent.children;
+output = parent.children[1];
+output = parent.children[1].innerText;
+output = parent.children[1].className;
+output = parent.children[1].nodeName;
+
+
+parent.children[1].innerText = "Child Two"
+parent.children[1].style.color = "red"
+
+
+parent.firstElementChild.innerText =  "Child One"
+parent.lastElementChild.innerText =  "Child Three"
+
+
+
+// Get Parent elements from a Child
+const child = document.querySelector(".child");
+output = child.parentElement;
+child.parentElement.style.border = "1px solid #ccc"
+child.parentElement.style.padding = "10px"
+
+
+
+
+// Sibling Elements
+const secondItem = document.querySelector(".child:nth-child(2)")
+output = secondItem
+output = secondItem.nextElementSibling
+
+secondItem.nextElementSibling.style.color = "orange"
+secondItem.previousElementSibling.style.color = "green"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(output)
