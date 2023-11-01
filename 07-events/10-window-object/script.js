75b9e8c865f1cfa631a953ac0edfdce7d7d66a51
Jason <jasonetienne1@gmail.com>
2023-11-01 19:16:37 +0000

    window object done

---
+++ 07-events/10-window-object/script.js
@@ -0,0 +1,38 @@
+
+
+// window.onload = function () {
+//     document.querySelector("h1").textContent = "Hello World";
+// }
+
+
+// window.addEventListener("load", ()=> document.querySelector("h1").textContent = "hello world2");
+window.addEventListener("load", ()=> console.log("Page Loaded"))
+
+
+//window.addEventListener("DOMContentLoaded", ()=> document.querySelector("h1").textContent = "hello world3");
+window.addEventListener("DOMContentLoaded", ()=> console.log("DOM Loaded"))
+
+
+
+console.log("run me")
+
+
+//document.querySelector("h1").innerText = "Hello World4"
+
+
+window.addEventListener("resize", ()=> {
+    document.querySelector("h1").innerText = `Resized to ${window.innerWidth} x ${window.innerHeight}`
+})
+
+
+
+window.addEventListener("scroll", ()=>{
+    console.log(`scrolled: ${window.scrollX} x ${window.scrollY}`);
+    if (window.scrollY > 1000) {
+        document.body.style.backgroundColor = "black"
+        document.body.style.color = "white"
+    } else {
+        document.body.style.backgroundColor = "white"
+        document.body.style.color = "black"
+    }
+})
